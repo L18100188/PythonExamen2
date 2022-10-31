50ec2c74b44f7a34df1e14b79cf166ef4ae13de4
HernandezLo <luishernandez0626@outlook.com>
2022-10-31 10:26:11 -0600

    P2

--- E2/mainapp/mainapp/urls.py
+++ E2/mainapp/mainapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gestorapp.urls')),
+    #path('',include('webapp.urls')),
     path('pelicula/', include('gestorapp.urls')),
     path('videojuego/',include('gestorapp.urls')),
     path('serie/',include('gestorapp.urls')),

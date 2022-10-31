a9fc9d027985d3bac45a37359306de420957b7b1
SamuelSolis11 <samuelsolis1183@gmail.com>
2022-10-31 10:43:02 -0600

    Actualizar

--- E1/consutaDao.py
+++
@@ -1,3 +0,0 @@
-class ConsultaDao:
-    _Consulta="SELECT SUM(costo) FROM contrato con JOIN contrato_persona cp ON (con.idcontrato=cp.idcontrato) JOIN persona per ON (per.idpersona=cp.idpersona) WHERE per.correo =samuel@gmail.com"
-
